8cce8e36f40058e3c10e5ee67f8b739c7107a937
samuel-grosz <groszs@stpaulsschool.org.uk>
2022-10-22 14:27:39 +0100

    Added functions and main python script to repo

---
+++ python-instagram-data/functions.py
@@ -0,0 +1,36 @@
+import instagram_private_api
+import json
+import codecs
+import datetime
+
+def from_json(json_object):
+    if '__class__' in json_object and json_object['__class__'] == 'bytes':
+        return codecs.decode(json_object['__value__'].encode(), 'base64')
+    return json_object
+
+def log(uname,pword):
+    with open("login_data.json") as file_data:
+        cached_settings = json.load(file_data, object_hook=from_json)
+
+    api = instagram_private_api.Client(
+        uname, pword,
+        settings=cached_settings)
+
+def getCommentors(user):
+    commentors = []
+    for i in api.user_feed(user):
+        for j in i['items']:
+            for k in j['caption']['preview_comments']:
+                commentors.append(k['user']['username'])
+    commentor_list = list(set(commentors))
+    for i in commentor_list:
+        if i == user: commentor_list.remove(i)
+    return commentor_list
+
+def writeCSV(objectofinterest):
+    with open("newer.csv", "a") as f:
+        try:
+            for i in getCommentors(objectofinterest):
+                f.write(objectofinterest + "," + i + "\n")
+        except:
+            pass
